21142cdc95cf5b3d5a9c7288ba3331a2fa8f897a
mroldan747 <mroldan747@gmail.com>
2020-04-07 22:06:50 +0200

    The user can register, can login and can logout

--- application.py
+++ application.py
@@ -1,9 +1,13 @@
 import os
 
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import login_required
 
 app = Flask(__name__)
 
@@ -25,10 +29,69 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
-@app.route("/")
+@app.route("/", methods = ["POST"])
+@login_required
 def index():
     return render_template("index.html")
 
-@app.route("/register")
+@app.route("/register", methods = ["GET", "POST"])
 def register():
-    return render_template("register.html")
+    if request.method == "POST":
+        if not request.form.get("username"):
+            return "must provide username"
+        if not request.form.get("password"):
+            return "must provide password"
+        if not request.form.get("password-confir"):
+            return "must confirm your password"
+        if request.form.get("password") != request.form.get("password-confir"):
+            return "The passwords are not the same"
+        username = request.form.get("username")
+        usr = db.execute("SELECT * FROM users WHERE user_name =:user_name", {"user_name":username}).fetchone()
+        
+
+        if usr is not None:
+            return "The username already exists"
+
+        psw =  generate_password_hash(request.form.get("password"))
+        db.execute("INSERT INTO users (user_name, hash) VALUES (:user_name, :hash)", {"user_name":username, "hash":psw})
+        db.commit()
+        return render_template("login.html")
+        
+    else:
+        return render_template("register.html")
+
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        #Check that the user has submitted the form with a username and a password
+        if not request.form.get("username"):
+            return "must provide username"
+        if not request.form.get("password"):
+            return "must provide password"
+        
+        username = request.form.get("username")
+        usr = db.execute("SELECT * FROM users WHERE user_name = :user_name", {"user_name":username}).fetchone()
+        psw = request.form.get("password")
+
+        if usr is None and not check_password_hash(usr[2], psw):
+            return "invalid username and/or password"
+        
+        session["user_id"] = usr[0]
+        return render_template("index.html")
+       
+    else:
+        return render_template("login.html")
+
+
+
+@app.route("/logout")
+def logout():
+    # Forget any user_id
+    session.clear()
+    return render_template("login.html")
---
+++ import.py
@@ -0,0 +1,27 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+import csv
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+
+f = open("books.csv")
+reader = csv.reader(f)
+
+
+db.execute("CREATE TABLE IF NOT EXISTS books (\
+            isbn varchar(15) NOT NULL,\
+            title TEXT NOT NULL,\
+            author TEXT NOT NULL,\
+            year INTEGER NOT NULL,\
+            PRIMARY KEY(isbn))")
+
+for isbn, title, author, year in reader:
+    db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)", {"isbn": isbn, "title": title, "author":author, "year": year})
+
+
+db.commit() # transactions are assumed, so close the transaction finished
+
